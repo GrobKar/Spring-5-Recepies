c27ce7da0eb1e8daf722da7bc1293a7d0ce7a62d
GrobKar <44326791+GrobKar@users.noreply.github.com>
2020-07-29 21:43:13 +0200

    Initial Commit

---
+++ src/main/java/guru/springframework/spring5recepies/Spring5RecepiesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recepies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecepiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecepiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5recepies/Spring5RecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recepies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecepiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
